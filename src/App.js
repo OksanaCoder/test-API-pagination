7bdbe5763d14698a8ce516141f84913b7eb0f322
OksanaCoder <oksishukh@yahoo.com>
2021-06-02 02:08:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardItem from './components/CardItem'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  const loadData = () => {
+    let url = 'https://5fe21e077a94870017682132.mockapi.io/api/testtask/doctors'
+    return new Promise(async (resolve, reject) => {
+      try {
+        let response = await axios.get(
+          url,
+          {},
+          { headers: new Headers({ 'Content-Type': 'application/json' }) }
+        )
+        resolve(response)
+      } catch (err) {
+        reject(err)
+      }
+    })
+      .then((response) => {
+        if (response.data) {
+          setData(response.data)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h3 style={{ fontWeight: 'bold' }} className='mt-4 mb-5 mainColor'>Наши специалисты</h3>
+        <p className='mainColor'>Высококвалифицированные врачи-стоматологи AstraDent регулярно посещают профессиональные
+        мастер-классы и стажируются в Украине и за рубежом, чтобы быть в курсе всех современных тенденций
+        дентальной медицины. Наши специалисты оснащены современной техникой мировых лидеров
+        производства стоматологического оборудования и используют только высококачественные
+        сертифицированные материалы от ведущих компаний.</p>
+        <Row>
+
+          {data.length > 0 ? (
+            <>
+              <CardItem data={data} />
+
+
+            </>
+          ) : (
+              <h4>No data</h4>
+            )}
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/CardItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Card, Col } from 'react-bootstrap'
+
+const CardItem = ({ data = [] }) => {
+    console.log(data, 'mapping')
+
+    return (
+        <>
+            {data.length && data.map(item => {
+                return (
+                    <Col lg={4} md={6} sm={12} xs={12} key={item.id} style={{ padding: '80px' }} onClick={() => window.open(item.link, "_blank")}>
+                        <Card style={{ border: 'none' }} className='text-center'>
+                            <Card.Img variant="top" src={item.avatar} style={{ borderRadius: '0' }} className='mb-4' />
+                            <Card.Body>
+                                <Card.Title>{item.lastName} {item.firstName}</Card.Title>
+                                <Card.Text>
+                                    <ul>
+                                        {Array.isArray(item.specialty) && item.speciality.length > 0 && item.speciality.map((i) => {
+                                            return (
+                                                <li className='lightGrey'>{i}</li>
+                                            )
+                                        })}
+                                    </ul>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )
+            })}
+        </>
+    )
+}
+
+export default CardItem
